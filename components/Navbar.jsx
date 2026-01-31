1d7457550ad9148d4de99524a7c7b206167c21aa
mr-banner <sahukamalkant28@gmail.com>
2026-01-31 21:12:50 +0530

    .

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,10 +3,12 @@
 import Cookies from "js-cookie";
 import { User } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Navbar() {
   const router = useRouter();
   const userName = Cookies.get("userName");
+  const [open, setOpen] = useState(false);
 
   const logout = () => {
     Cookies.remove("token");
@@ -17,31 +19,25 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center inline-padding p-4 bg-[#E1E9EE]">
       <span className="font-semibold sm:text-3xl text-lg">{userName}</span>
-      <div className="relative group">
+
+      <div className="relative">
         <button
+          onClick={() => setOpen(!open)}
           className="p-2 rounded-full cursor-pointer hover:bg-gray-200 transition"
-          aria-haspopup="true"
         >
           <User />
         </button>
-        <div
-          className="
-        absolute right-0  w-32
-        bg-white border border-gray-200 rounded-md shadow-lg
-        opacity-0 scale-95 pointer-events-none
-        group-hover:opacity-100 group-hover:scale-100 group-hover:pointer-events-auto
-        transition-all duration-150 ease-out
-        z-50
-      "
-        >
-          <button
-            onClick={logout}
-            className="w-full text-left px-4 py-2 text-sm
-                   hover:bg-gray-100 transition cursor-pointer"
-          >
-            Logout
-          </button>
-        </div>
+
+        {open && (
+          <div className="absolute -right-10 w-32 bg-white rounded-md shadow-lg z-50">
+            <button
+              onClick={logout}
+              className="w-full text-left px-4 py-2 text-sm hover:bg-gray-100 cursor-pointer"
+            >
+              Logout
+            </button>
+          </div>
+        )}
       </div>
     </nav>
   );
